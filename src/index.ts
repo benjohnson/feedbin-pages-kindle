5ca5cbf2ff3eb697555321be2f4bac33a17991a6
Ben Johnson <ben@benjohnson.ca>
2019-10-13 17:57:23 -0400

    Correct table of contents format

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
 import fs from 'fs';
 import ejs from 'ejs';
+import del from 'del';
 import log from './log';
 import generateEntry from './generateEntry';
 import * as types from './types';
 
 async function main() {
   try {
+    log('Removing book directory...');
+    await del(['book/**']);
 
     const entries: types.Entry[] = JSON.parse(fs.readFileSync('entries.json', 'utf-8'));
 
     for (let entry of entries) {
+      log(`Generating entry: "${entry.title}"`);
       await generateEntry(entry);
     }
 
     const templates = fs.readdirSync('templates').filter(x => x !== 'entry.html');
     for (let template of templates) {
       log(`Generating template: ${template}`);
+      const today = new Date();
+      const shortDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
       const templateContents = fs.readFileSync(`templates/${template}`, 'utf-8');
-      fs.writeFileSync(`book/${template}`, ejs.render(templateContents, { entries }));
+      fs.writeFileSync(`book/${template}`, ejs.render(templateContents, { entries, shortDate }));
     }
 
   } catch (err) {
